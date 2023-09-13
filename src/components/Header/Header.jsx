e986cee1a0c9a4f843e42e0ec3e8454f20d6d5ac
Atikur R Shanto <atikur.r.shanto@gmail.com>
2023-09-13 09:57:23 +0600

    Start React Knowlede Cafe

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-2xl font-bold text-center underline mt-2'>Welcome React Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
